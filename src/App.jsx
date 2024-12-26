4abc7edf4baa658129364fd3481bad74fed09124
unknown <prashant.sharma@tecdune.com>
2024-12-26 18:59:49 +0530

    create folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
-	<h1 className="text-3xl font-bold underline">
-		Testing!
-	</h1>
+	<>
+		<h1 className="text-3xl font-bold underline">
+			Testing!
+		</h1>
+		<Footer></Footer>
+
+	</>
   )
 }
 
